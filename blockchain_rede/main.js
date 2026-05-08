88737fdc96dc04b5d862120a6b3604319c7f9f78
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-05-08 18:01:29 -0300

    Add files via upload

--- blockchain_rede/main.js
+++ blockchain_rede/main.js
@@ -2,7 +2,7 @@ const bloco_template = document.getElementById("bloco-template");
 const minerar_btn = document.querySelectorAll(".minerar-btn");
 const novo_bloco_btn = document.getElementById("novo-bloco-btn");
 
-const assinatura = "0000";
+const assinatura = "faca";
 
 
 const worker = new Worker('../worker.js', { type: 'module' });
@@ -18,25 +18,43 @@ function change_background_color(e, hash){
 }
 
 
+function check_chain(node_letter){
+    last_block = parseInt(document.querySelector(`#novo-bloco-btn[data-row="${node_letter}"]`).dataset.col);
+    curr_block = 0;
+    
+    while (++curr_block < last_block){
+        hash_atual = document.querySelector(`.hash-output#${node_letter}${curr_block}`).value;
+        hash_previa  = document.querySelector(`.previous-hash-output#${node_letter}${curr_block+1}`).value;
+
+        if (hash_atual != hash_previa) {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+
 var current_block;
+var current_node;
 function minerar(e){
     document.querySelectorAll('button').forEach(b => {
         b.disabled = true;
     });
 
-    current_block =  e.currentTarget.id;
-    any_previous_hash = document.querySelector(`#${CSS.escape(parseInt(current_block, 10)-1)} .hash-output`);
+    current_node  = e.currentTarget.id[0];
+    current_block = parseInt(e.currentTarget.id.slice(1));
+
+    any_previous_hash = document.querySelector(`#${current_node}${current_block-1} .hash-output`);
     if (any_previous_hash != null){
-        document.querySelector(`#${CSS.escape(current_block)} .previous-hash-output`).value = any_previous_hash.value;
-        change_background_color(document.querySelector(`#${CSS.escape(current_block)} .previous-hash-output`), any_previous_hash.value);
+        document.querySelector(`#${current_node}${current_block} .previous-hash-output`).value = any_previous_hash.value;
+        change_background_color(document.querySelector(`#${current_node}${current_block} .previous-hash-output`), any_previous_hash.value);
     }
-    const previous_hash_value = document.querySelector(`#${CSS.escape(current_block)} .previous-hash-output`).value;
-    const data_input = document.querySelector(`#${CSS.escape(current_block)} .data-input`).value;
 
 
     const dados = {
-        previous_hash_value: document.querySelector(`#${CSS.escape(current_block)} .previous-hash-output`).value,
-        data_input: document.querySelector(`#${CSS.escape(current_block)} .data-input`).value,
+        previous_hash_value: document.querySelector(`#${current_node}${current_block} .previous-hash-output`).value,
+        data_input: document.querySelector(`#${current_node}${current_block} .data-input`).value,
         assinatura: assinatura,
     }
     worker.postMessage(dados);
@@ -45,30 +63,21 @@ function minerar(e){
 worker.onmessage = e => {
     const {hash, nonce} = e.data;
 
-    document.querySelector(`#${CSS.escape(current_block)} .hash-output`).value = hash;
-    change_background_color(document.querySelector(`#${CSS.escape(current_block)} .hash-output`), hash);
-    document.querySelector(`#${CSS.escape(current_block)} .nonce-output`).value = nonce.toString();
-
-    const letra = current_block[0];
-    const work_done_ta = document.getElementById(`${letra}work`);
-    const total_work = nonce + parseInt(work_done_ta.dataset.work);
-
-    work_done_ta.value = `Trabalho total: ${total_work}`;
-    work_done_ta.setAttribute('data-work', total_work);
-    
-
-
+    document.querySelector(`#${current_node}${current_block} .hash-output`).value = hash;
+    change_background_color(document.querySelector(`#${current_node}${current_block} .hash-output`), hash);
+    document.querySelector(`#${current_node}${current_block} .nonce-output`).value = nonce.toString();
 
     document.querySelectorAll('button').forEach(b => {
         b.disabled = false;
     });
+
+    document.getElementById(`${current_node}node`).dataset.broken = check_chain(current_node);
 };
 
 
 
 document.querySelectorAll("#novo-bloco-btn").forEach(b => {
     b.addEventListener('click', (e) => {
-
         const block_ltr = e.target.dataset.row;
         const block_num = parseInt(e.target.dataset.col);
 
@@ -76,7 +85,6 @@ document.querySelectorAll("#novo-bloco-btn").forEach(b => {
         const block_id = `${block_ltr}${block_num+1}`;
 
         e.target.setAttribute('data-col', block_num+1);
-
 
         const f = document.createDocumentFragment();
         const node = bloco_template.content.cloneNode(true);
@@ -98,11 +106,9 @@ document.querySelectorAll("#novo-bloco-btn").forEach(b => {
         node.querySelector(".minerar-btn").addEventListener('click', minerar);
 
         f.appendChild(node);
-        f.append(document.createElement('br'), document.createElement('br'));
         b.before(f);
     });
 })
-
 
 
 
